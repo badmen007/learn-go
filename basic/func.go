668cc6bb701de43acf66d9bc74a85a739980d729
badman007 <1572226525@qq.com>
2022-08-06 17:23:46 +0800

    函数式一等公民

---
+++ basic/func.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"reflect"
+	"runtime"
+)
+
+func eval(a, b int, op string) (int, error) {
+	switch op {
+	case "+":
+		return a + b, nil
+	case "-":
+		return a - b, nil
+	case "*":
+		return a * b, nil
+	case "/":
+		q, _ := div(a, b)
+		return q, nil
+	default:
+		return 0, fmt.Errorf("unsupported operation: %s", op)
+	}
+}
+
+// 可以返回多个值
+func div(a, b int) (q, r int) {
+	return a / b, a % b
+}
+
+func apply(op func(int, int) int, a, b int) int {
+	//拿到函数的名字
+	p := reflect.ValueOf(op).Pointer()
+	opName := runtime.FuncForPC(p).Name()
+	fmt.Printf("Calling function %s with args "+"(%d, %d)\n", opName, a, b)
+	return op(a, b)
+}
+
+func pow(a, b int) int {
+	return int(math.Pow(float64(a), float64(b)))
+}
+
+// 传递可变参数的列表
+func sum(numbers ...int) int {
+	s := 0
+	for i := range numbers {
+		s += numbers[i]
+	}
+	return s
+}
+
+func main() {
+	if result, error := eval(3, 4, "x"); error != nil {
+		fmt.Println("Error:", error)
+	} else {
+		fmt.Println(result)
+	}
+	q, r := div(13, 3)
+	fmt.Println(q, r)
+
+	fmt.Println(apply(pow, 3, 4))
+	sum(1, 2, 3, 4, 5, 6)
+}
